d8e988dd762d7f06aec54ae3732f3b1f11381a47
gary909 <garywhite61@hotmail.com>
2022-11-17 12:57:41 +0100

    code working

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-function check(a, x) { };
+function check(a, x) {
+    return a.indexOf(x) >= 0;
+    // return a.includes[x];
+};
 
 console.log(check([66, 101], 66)); // 66 - True
 console.log(check([80, 117, 115, 104, 45, 85, 112, 115], 45)); // True
